d4067566ca76558a92ccd1bbdf8d2ea9d2368c10
DaniT128 <a.23308051281325@cbtis128.edu.mx>
2024-10-30 15:15:03 -0600

    Add files via upload

---
+++ practica 2.py
@@ -0,0 +1,8 @@
+print() #para dejar un espacio al momento de ejecutar 
+print("jose daniel arguijo torres")
+print() #par dejar un espacio al momento de ejecutar 
+
+def my_funtion():  #indica definir una funcion 
+    print("hola mundo fiel") #ingresa el nombre de una persona 
+print(" ")#para dejar un espacio al momento de ejecutar 
+my_funtion()    #indica la funcion de la persona
